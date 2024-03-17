0a49dad396f098bd41051f583cf1f94468aed0c1
Shaun O'Connell <ndorfin@gmail.com>
2024-03-17 21:03:07 +1300

    Add minified icons for the Bridge section

--- _data/icons.mjs
+++ _data/icons.mjs
@@ -42,5 +42,41 @@ export const ICON_LIST = [
 		label: 'HMG Emplacement',
 		group_id: 'bridge',
 		filename: 'HMG Emplacement',
-	}
+	},
+	{
+		id: 'orbital_ems_strike',
+		label: 'Orbital EMS Strike',
+		group_id: 'bridge',
+		filename: 'Orbital EMS Strike',
+	},
+	{
+		id: 'orbital_gas_strike',
+		label: 'Orbital Gas Strike',
+		group_id: 'bridge',
+		filename: 'Orbital Gas Strike',
+	},
+	{
+		id: 'orbital_precision_strike',
+		label: 'Orbital Precision Strike',
+		group_id: 'bridge',
+		filename: 'Orbital Precision Strike',
+	},
+	{
+		id: 'orbital_smoke_strike',
+		label: 'Orbital Smoke Strike',
+		group_id: 'bridge',
+		filename: 'Orbital Smoke Strike',
+	},
+	{
+		id: 'shield_generator_relay',
+		label: 'Shield Generator Relay',
+		group_id: 'bridge',
+		filename: 'Shield Generator Relay',
+	},
+	{
+		id: 'tesla_tower',
+		label: 'Tesla Tower',
+		group_id: 'bridge',
+		filename: 'Tesla Tower',
+	},
 ]
