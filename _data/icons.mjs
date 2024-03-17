51648c3643e0fc6f673c5547e80cdc1f4579aa26
Shaun O'Connell <ndorfin@gmail.com>
2024-03-17 20:45:31 +1300

    Create a rudimentary test environment for each icon

---
+++ _data/icons.mjs
@@ -0,0 +1,46 @@
+export const GROUP_LIST = [
+	{
+		id: 'bridge',
+		label: 'Bridge',
+		foldername: 'Bridge',
+	},
+	{
+		id: 'engineering_bay',
+		label: 'Engineering Bay',
+		foldername: 'Engineering Bay',
+	},
+	{
+		id: 'general_stratagems',
+		label: 'General Stratagems',
+		foldername: 'General Stratagems',
+	},
+	{
+		id: 'hangar',
+		label: 'Hangar',
+		foldername: 'Hangar',
+	},
+	{
+		id: 'orbital_cannons',
+		label: 'Orbital Cannons',
+		foldername: 'Orbital Cannons',
+	},
+	{
+		id: 'patriotic_administration_center',
+		label: 'Patriotic Administration Center',
+		foldername: 'Patriotic Administration Center',
+	},
+	{
+		id: 'robotics_workshop',
+		label: 'Robotics Workshop',
+		foldername: 'Robotics Workshop',
+	},
+];
+
+export const ICON_LIST = [
+	{
+		id: 'hmg_emplacement',
+		label: 'HMG Emplacement',
+		group_id: 'bridge',
+		filename: 'HMG Emplacement',
+	}
+]
